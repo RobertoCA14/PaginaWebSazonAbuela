3d30bdcdfcd69a67b2c49986ddf3fea63addfed5
Roberto Cuevas <111618997+RobertoCA14@users.noreply.github.com>
2024-12-13 01:42:40 +0000

    precio

--- src/components/Store.js
+++ src/components/Store.js
@@ -7,13 +7,13 @@ const products = [
   {
     id: 1,
     name: "El Sazón de la Abuela, 12 oz, Ref.772",
-    price: 13.8,
+    price: 140,
     image: producto1,
   },
   {
     id: 2,
     name: "El Sazón de la Abuela, 24 oz, Ref.7302",
-    price: 17.4,
+    price: 180,
     image: producto2,
   },
 ];
