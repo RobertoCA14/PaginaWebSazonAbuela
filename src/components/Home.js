0c4d2df3ce58df8123c402e9eb30d1087e78f1b1
Roberto Cuevas <111618997+RobertoCA14@users.noreply.github.com>
2024-12-13 01:34:56 +0000

    err

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,9 @@ import React from "react";
 import Carousel from "./Carousel";
 import logo2 from "../assets/logo2.png";
 import logo1 from "../assets/logo1.png";
+import Fotosingredientes from "../assets/Fotosingredientes.png";
+import LogoSazonAbuela from "../assets/LogoSazonAbuela.png";
+
 function HomePage() {
   return (
     <div className="homepage">
@@ -17,16 +20,24 @@ function HomePage() {
             <h2 className="text-3xl font-semibold mb-4">
               ¿Por qué elegir nuestro sazón?
             </h2>
-            <h2 className="text-4xl font-bold mb-4">LA FAMILIA FUSIÓN</h2>
+            <h2 className="text-4xl font-bold mb-4">EL SAZON DE LA ABUELA</h2>
             <p className="mb-6">
-              En Fusión Criolla, nos enorgullecemos de transformar cada comida
-              con nuestro sazón criollo en una auténtica celebración de sabor y
-              tradición. Nuestro compromiso con la excelencia culinaria nos
-              distingue como verdaderos expertos en sazón criollo, elevando el
-              arte de cocinar con los ingredientes más frescos y las técnicas
-              más auténticas. Nuestro sazón está elaborado con los mejores
-              ingredientes naturales y una receta tradicional que resalta el
-              sabor de cada plato.
+              En un mundo acelerado, encontrar el equilibrio entre una
+              alimentación saludable y deliciosa puede ser todo un desafío. 'El
+              Sazón de la Abuela' te ofrece una solución práctica y nutritiva.
+              Nuestro sazón líquido natural, elaborado con ingredientes frescos
+              y sin aditivos artificiales, realza el sabor de tus platos sin
+              comprometer tu bienestar. Rico en vitaminas, minerales y
+              antioxidantes, nuestro producto te ayuda a cuidar de tu salud
+              mientras disfrutas de comidas sabrosas y variadas. Además, su
+              versatilidad te permite utilizarlo en una amplia gama de recetas,
+              desde sopas y guisos hasta marinados y aderezos, ahorrándote
+              tiempo y esfuerzo en la cocina.lo largo de este documento,
+              exploraremos los fundamentos de nuestra sazón líquido natural, sus
+              beneficios nutricionales, y cómo puede integrarse fácilmente en
+              diversas preparaciones culinarias. Descubre cómo el "Sazón Fusión
+              criolla" puede transformar tu experiencia en la cocina, ofreciendo
+              una nueva dimensión de sabor y bienestar.
             </p>
             <button className="bg-white text-red-700 px-6 py-3 rounded-full font-semibold hover:bg-gray-100 transition">
               Leer más
@@ -37,7 +48,7 @@ function HomePage() {
         {/* Imagen de logo debajo de la sección roja */}
         <div className="text-center p-8">
           <img
-            src={logo2}
+            src={LogoSazonAbuela}
             alt="Imagen de Sazón Criollo"
             className="mx-auto mb-1 w-64 h-36 object-cover rounded"
           />
@@ -45,33 +56,45 @@ function HomePage() {
 
         {/* Características del producto */}
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6 px-4">
-          <div className="bg-gray-300 p-4 rounded shadow-lg">
-            <h3 className="text-xl font-bold mb-2">Ingredientes Naturales</h3>
-            <p>Solo ingredientes frescos y sin aditivos.</p>
+          <div className="bg-gray-300 p-4 rounded shadow-lg text-center">
+            <h3 className="text-xl font-bold mb-2">
+              Ingredientes Naturales del "Sazon de la Abuela"
+            </h3>
+            <p>
+              <p>
+                verduras, aji cubanela, aji morron, apio, oregano, aji gustoso,
+                ajo, aceite, verde, agua, sal, cilatro, pimienta.
+              </p>
+            </p>
+            <div className="text-center p-8">
+              <img
+                src={Fotosingredientes}
+                alt="Imagen de Sazón Criollo"
+                className="mx-auto mb-4 w-full max-w-lg h-auto object-cover rounded-lg"
+              />
+            </div>
           </div>
-          <div className="flex items-center p-4 border rounded shadow-lg">
+
+          <div className="bg-gray-300 p-4 rounded shadow-lg text-center">
             <div
-              className="w-1/3 h-32 bg-cover bg-center rounded-lg mr-4"
+              className="w-full h-32 bg-cover bg-center rounded-lg mb-4"
               style={{
                 backgroundImage: `url(${logo1})`,
               }}
             ></div>
-            <div className=" w-2/3">
-              <h3 className="text-xl font-bold mb-2">Auténtico Sabor</h3>
-              <p>Inspirado en la esencia de la cocina criolla.</p>
-            </div>
+            <h3 className="text-xl font-bold mb-2">Auténtico Sabor</h3>
+            <p>Inspirado en la esencia de la cocina criolla.</p>
           </div>
-          <div className="flex items-center p-4 border rounded shadow-lg">
+
+          <div className="bg-gray-300 p-4 rounded shadow-lg text-center">
             <div
-              className="w-1/3 h-32 bg-cover bg-center rounded-lg mr-4"
+              className="w-full h-32 bg-cover bg-center rounded-lg mb-4"
               style={{
                 backgroundImage: `url(${logo1})`,
               }}
             ></div>
-            <div className="w-2/3  ">
-              <h3 className="text-xl font-bold mb-2">Fácil y Versátil</h3>
-              <p>Perfecto para todo tipo de platos.</p>
-            </div>
+            <h3 className="text-xl font-bold mb-2">Fácil y Versátil</h3>
+            <p>Perfecto para todo tipo de platos.</p>
           </div>
         </div>
       </section>
